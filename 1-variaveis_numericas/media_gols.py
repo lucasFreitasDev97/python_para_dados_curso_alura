95360f2227f6527b738d5fb3fcd692fb8133e10e
Lucas Freitas <lucas.rodrigues.developer97@gmail.com>
2026-05-28 19:52:18 -0300

    first commit

---
+++ 1-variaveis_numericas/media_gols.py
@@ -0,0 +1,23 @@
+# O técnico do Vasco quer um relatório do desempenho do time. 
+# Ele te deu os dados de gols marcados nos últimos quatro jogos do campeonato. 
+# Calcule o indicador correto para mostrar o rendimento do time.
+
+print('============================================')
+print('Desempenho em Gols - Vasco da Gama (4 Jogos)')
+print('============================================')
+
+# Dados do Sistema
+gols_jogo_1 = 2
+gols_jogo_2 = 0
+gols_jogo_3 = 3
+gols_jogo_4 = 1
+
+print('Jogo 01: ', gols_jogo_1)
+print('Jogo 02: ', gols_jogo_2)
+print('Jogo 03: ', gols_jogo_3)
+print('Jogo 04: ', gols_jogo_4)
+
+media_gols = (gols_jogo_1 + gols_jogo_2 + gols_jogo_3 + gols_jogo_4) / 4
+print('Média de gols por jogo: ', media_gols)
+
+
